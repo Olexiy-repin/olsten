c2eb1d0a778ff24a3cdee2d5ba11a8848aa26926
Olexiy repin <repinlyoha@gmail.com>
2018-07-30 02:05:00 +0300

    finish landing

--- src/js/form-modal.js
+++ src/js/form-modal.js
@@ -8,22 +8,12 @@ $('.popup-with-form').magnificPopup({
 		closeOnContentClick: true,
 		closeBtnInside: false,
 		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		mainClass: 'mfp-no-margins mfp-with-zoom',
 		image: {
 			verticalFit: true
 		},
 		zoom: {
 			enabled: true,
-			duration: 300 // don't foget to change the duration also in CSS
+			duration: 300
 		}
 	});
-
-// $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-//         disableOn: 700,
-//         type: 'iframe',
-//         mainClass: 'mfp-fade',
-//         removalDelay: 160,
-//         preloader: false,
-//
-//         fixedContentPos: false
-//     });
